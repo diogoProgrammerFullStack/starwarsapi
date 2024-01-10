d99b8ea5bd28306f473122a4e879b849b41b8b74
Diogo <diogoprogrammerfullstack@gmail.com>
2024-01-10 01:41:01 -0300

    first commit

---
+++ star-wars/src/main/java/br/com/starwars/config/EndPoint.java
@@ -0,0 +1,13 @@
+package br.com.starwars.config;
+
+/**
+ * 
+ * @author Diogo Oliveira
+ *
+ */
+public class EndPoint {
+	
+  public static final String BASE_URL_PEOPLE = "https://swapi.co/api/people/1/";
+  public static final String BASE_URL_PLANETS = "https://swapi.co/api/planets/1/";
+
+}
---
+++ star-wars/src/main/java/br/com/starwars/config/Token.java
@@ -0,0 +1,12 @@
+package br.com.starwars.config;
+
+/**
+ * 
+ * @author Diogo Oliveira
+ *
+ */
+public class Token {
+	
+	public static final String TOKEN = "recebe o token";
+
+}
